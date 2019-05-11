f21b6465870f7b465b4b19610a6e7bb236623483
Peter Wagner <peterwagner@live.co.uk>
2019-05-11 09:04:44 +0100

    * Build 973

--- Source/Krypton Components/ComponentFactory.Krypton.Design/Properties/AssemblyInfo.cs
+++ Source/Krypton Components/ComponentFactory.Krypton.Design/Properties/AssemblyInfo.cs
@@ -5,15 +5,15 @@
 //  proprietary information of Component Factory Pty Ltd, 13 Swallows Close, 
 //  Mornington, Vic 3931, Australia and are supplied subject to licence terms.
 // 
-//  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV) 2017 - 2019. All rights reserved. (https://github.com/Wagnerp/Krypton-NET-5.470)
+//  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV) 2017 - 2019. All rights reserved. (https://github.com/Wagnerp/Krypton-NET-5.451)
 //  Version 4.7.0.0  www.ComponentFactory.com
 // *****************************************************************************
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("5.470.0.0")]
-[assembly: AssemblyFileVersion("5.470.564.0")]
-[assembly: AssemblyInformationalVersion("5.470.564.0")]
+[assembly: AssemblyVersion("5.451.0.0")]
+[assembly: AssemblyFileVersion("5.451.564.0")]
+[assembly: AssemblyInformationalVersion("5.451.564.0")]
 [assembly: AssemblyCopyright("© Component Factory Pty Ltd, 2006-2019. Then modifications by Peter Wagner (aka Wagnerp) & Simon Coghlan (aka Smurf-IV) 2017-2019. All rights reserved.")]
 [assembly: AssemblyProduct("Krypton Design")]
 [assembly: AssemblyDefaultAlias("ComponentFactory.Krypton.Design.dll")]
